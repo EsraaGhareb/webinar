35ca6e485eef0c8395a81469f2412d08a6f88ec0
DESKTOP-6CU6AEH\Esraa Ghareb <esraagharebmohamed@gmail.com>
2021-04-15 15:17:53 +0200

    Profile page

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,5 +136,16 @@ if ($("body").css("direction") == "rtl") {
       }
     });
   });
-
 })(jQuery);
+// profile image preview
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#img-preview').attr('src', e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
